0be463f27be30b3601563bd3c9c39353dcd36097
Mohammad Noor <git@mdsalih.com>
2013-11-10 15:45:50 +0000

    Added jsdoc for ipcstr2arraybuffer

--- IPCWebParse/www/js/ipcStrParse.js
+++ IPCWebParse/www/js/ipcStrParse.js
@@ -1,3 +1,11 @@
+/**
+* Function takes an IPC message in string format and returns 
+* a binary ArrayBuffer representation of the IPC string.
+* Can generate an input string with the following code in a
+* kdb process: raze string -8!<code>. eg. raze string -8!1+1
+* @param {string} IPC message as a string.
+* @returns {ArrayBuffer} binary reprensetation of IPC string
+*/
 function ipcstr2arraybuffer(str){
   var buffer = new ArrayBuffer(str.length/2);
   var bufferView = new Uint8Array(buffer);
